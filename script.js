3d602550555356b6b44ab18a6d0f2da17bd06f79
Aman Khajuria <24khajuriaa@gmail.com>
2026-03-12 17:19:00 -0700

    Implementing the console game a new way
    Instead of using prompts which I will have to eventually change, I am Implementing a playRound public function which takes an index as the parameter, which will be used later on when the DOM gets involved
    Having trouble with the players factory function, but will fix soon

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ const gameBoard = (()=> {
 const players = (() => {
 
     const Player = (isTurn) => {
-        const switchTurn = (isTurn) => !isTurn;
+        this.isTurn = isTurn;
+        const switchTurn = () => this.isTurn = !isTurn;
         const getIsTurn = () => isTurn;
 
         return {switchTurn, getIsTurn};
@@ -28,21 +29,40 @@ const playGame = (() => {
     let player1 = players.Player(true);
     let player2 = players.Player(false);
 
-    gameBoard.display();
+    let playRound = (index) => {
 
-    if (player1.getIsTurn() == true) {
-        console.log("It's player1's turn");
-        let input = prompt("Player1, Choose where you want to place your x!");
-        gameBoard.gameBoard[parseInt(input)] = "X";
+        if (player1.getIsTurn() == true) {
+            console.log("It's player1's turn");
 
-        player1.switchTurn();
-        player2.switchTurn();
+            gameBoard.gameBoard[parseInt(index)] = "X";
 
-        gameBoard.display();
+            if (index > 8) {
+                throw console.error("Value is outside of the 0-8 length index");
+            }
+
+            player1.switchTurn();
+            player2.switchTurn();
+
+            gameBoard.display();
+        }
+
+        // Play Round function switches between player 1 and 2 based off the turn attribute
+
+        if (player2.getIsTurn() == true) {
+            console.log("It's player2's turn");
+            
+            gameBoard.gameBoard[parseInt(index)] = "O";
+
+            if (index > 8) {
+                throw console.error("Value is outside of the 0-8 length index");
+            }
+
+            player1.switchTurn();
+            player2.switchTurn();
+
+            gameBoard.display();
+        }
     }
 
-    if (player2.getIsTurn() == true) {
-        console.log("It's player2's turn");
-        input = prompt("Player2, choose where you want to place your O!");
-    }
+    return {playRound};
 })();
