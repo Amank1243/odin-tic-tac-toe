e6be30a4c06047cc7bd3faa7adfa7f0b0422c39d
Aman Khajuria <24khajuriaa@gmail.com>
2026-03-15 15:45:54 -0700

    Got a working tic tac toe game on the console The next step is to get the DOM involved

--- script.js
+++ script.js
@@ -25,59 +25,90 @@ const players = (() => {
 
 const playGame = (() => {
 
-    const checkForWin = () => {
-
-    };
-
     let player1 = players.Player(true);
     let player2 = players.Player(false);
+
+    let gameOver = false;
+
+    const winningCombos = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [2,4,6],
+        [0,4,8]
+    ];
+
+     const checkForWin = (board) => {
+        for (const [indexA, indexB, indexC] of winningCombos) {
+            
+            if (board[indexA] == "O" && board[indexB] == "O" && board[indexC] == "O") {
+                console.log("Winner!");
+                gameOver = true;
+                break;
+            } else if (board[indexA] == "X" && board[indexB] == "X" && board[indexC] == "X") {
+                console.log("Winner!");
+                gameOver = true;
+                break;
+            }
+        }
+    };
 
     let playRound = (index) => {
 
         if (player1.getIsTurn() == true) {
+
+            if (gameOver == true) {
+                console.log("The game has ended");
+                return;
+           };
+
             console.log("It's player1's turn");
 
              if (parseInt(index) > 8) {
                 throw new Error("Value is outside of the gameboard");
-            }
-
-            console.log("Throw should stop this message for player 1");
-
-            if (gameBoard.gameBoard[index].includes("O") || gameBoard.gameBoard[index].includes("X")) {
-                throw console.error("Space already has a marker on it");
-            }
-
-            gameBoard.gameBoard[parseInt(index)] = "X";
-
-           
-
-            player1.switchTurn();
-            player2.switchTurn();
-
-            gameBoard.display();
-        } else if (player2.getIsTurn() == true) {
-            console.log("It's player2's turn");
-            
-            if (parseInt(index) > 8) {
-                throw console.error("Value is outside of the 0-8 length index");
             };
 
             if (gameBoard.gameBoard[index].includes("O") || gameBoard.gameBoard[index].includes("X")) {
                 throw new Error("Space already has a marker on it");
-            }
+            };
 
-            gameBoard.gameBoard[parseInt(index)] = "O";
+            gameBoard.gameBoard[parseInt(index)] = "X";
+            gameBoard.display();
+           
+            checkForWin(gameBoard.gameBoard);
 
             player1.switchTurn();
             player2.switchTurn();
 
+            
+        } else if (player2.getIsTurn() == true) {
+
+             if (gameOver == true) {
+                console.log("The game has ended");
+                return;
+            };
+
+            console.log("It's player2's turn");
+            
+            if (parseInt(index) > 8) {
+                throw new Error("Value is outside of the 0-8 length index");
+            };
+
+            if (gameBoard.gameBoard[index].includes("O") || gameBoard.gameBoard[index].includes("X")) {
+                throw new Error("Space already has a marker on it");
+            };
+
+            gameBoard.gameBoard[parseInt(index)] = "O";
             gameBoard.display();
-        }
 
-        // Play Round function switches between player 1 and 2 based off the turn attribute
+            checkForWin(gameBoard.gameBoard);
 
-        
-    }
-
+            player1.switchTurn();
+            player2.switchTurn();
+        };
+    };
     return {playRound};
 })();
