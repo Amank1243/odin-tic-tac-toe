b8a0f451a28c65245e7a554efd98338171ce0a7a
Aman Khajuria <24khajuriaa@gmail.com>
2026-03-16 15:15:31 -0700

    Added working DOM tic tac toe game
    Next step is to implement changing names, and a restart button which works without refreshing the page

--- script.js
+++ script.js
@@ -5,10 +5,7 @@
 const gameBoard = (()=> {
     let gameBoard = new Array(9).fill(" ");
 
-    const grid = document.querySelector(".gameboard");
-
-    
-
+    const grid = document.querySelector(".gameboard");    
     let gridSquares = [];
     
 
@@ -16,23 +13,12 @@ const gameBoard = (()=> {
         for (const item of gameBoard) {
             const square = document.createElement("div");
             square.className = "square";
+            square.textContent = item;
 
             gridSquares.push(square);
             grid.appendChild(square);
         };
     })();
-
-    const getIndexOfSquare = () => {
-            let index = 0
-            for (const square of gridSquares) {
-                square.addEventListener("click", ()=>{
-                    index = gridSquares.indexOf(square);
-                    console.log(index);
-                });
-            }
-
-            return index;
-        };
 
     const display = () => {
         console.log('|' +gameBoard[0] +'|' + gameBoard[1] + '|' + gameBoard[2] + '|' +'\n' +
@@ -40,7 +26,7 @@ const gameBoard = (()=> {
                     '|' + gameBoard[6] + '|' + gameBoard[7] + '|' + gameBoard[8] + '|' + '\n')
     };
 
-    return {gameBoard, display, getIndexOfSquare};
+    return {gameBoard, display, gridSquares};
 })();
 
 // 
@@ -48,9 +34,12 @@ const gameBoard = (()=> {
 // 
 
 const players = (() => {
+
     const Player = (isTurn) => {
         const switchTurn = () => isTurn = !isTurn;
         const getIsTurn = () => isTurn;
+
+        let playerName = " "; // Implement this when you need to add player names
 
         return {switchTurn, getIsTurn};
     }
@@ -66,6 +55,8 @@ const players = (() => {
 const playGame = (() => {
     let player1 = players.Player(true);
     let player2 = players.Player(false);
+
+    const gameText = document.querySelector("p.gameText")
 
     let gameOver = false;
 
@@ -84,34 +75,35 @@ const playGame = (() => {
         for (const [indexA, indexB, indexC] of winningCombos) {
             
             if (board[indexA] == "O" && board[indexB] == "O" && board[indexC] == "O") {
-                console.log("Winner!");
+                gameText.textContent = "Winner!";
                 gameOver = true;
                 break;
             } else if (board[indexA] == "X" && board[indexB] == "X" && board[indexC] == "X") {
-                console.log("Winner!");
+                gameText.textContent = "Winner!";
                 gameOver = true;
                 break;
             }
         }
     };
 
-    let playRound = () => {
-        index = getIndexOfSquare();
-        
+    let playRound = (index) => {
+       
         if (player1.getIsTurn() == true) {
 
             if (gameOver == true) {
-                console.log("The game has ended");
+                 gameText.textContent = "The game has ended";
                 return;
            };
 
-            console.log("It's player1's turn");
+             gameText.textContent = "It's player2's turn";
 
              if (parseInt(index) > 8) {
+                 gameText.textContent = "Value is outside of the gameboard";
                 throw new Error("Value is outside of the gameboard");
             };
 
             if (gameBoard.gameBoard[index].includes("O") || gameBoard.gameBoard[index].includes("X")) {
+                 gameText.textContent = "Space already has a marker on it";
                 throw new Error("Space already has a marker on it");
             };
 
@@ -126,17 +118,19 @@ const playGame = (() => {
             
         } else if (player2.getIsTurn() == true) {
              if (gameOver == true) {
-                console.log("The game has ended");
+                gameText.textContent = "The game has ended";
                 return;
             };
 
-            console.log("It's player2's turn");
+            gameText.textContent = "It's player1's turn";
             
             if (parseInt(index) > 8) {
+                gameText.textContent = "Value is outside of the 0-8 length index";
                 throw new Error("Value is outside of the 0-8 length index");
             };
 
             if (gameBoard.gameBoard[index].includes("O") || gameBoard.gameBoard[index].includes("X")) {
+                gameText.textContent = "Space already has a marker on it"
                 throw new Error("Space already has a marker on it");
             };
 
@@ -149,5 +143,14 @@ const playGame = (() => {
             player2.switchTurn();
         };
     };
-    // return {playRound};
+
+    // Calls the playRound function when square is clicked
+
+    for (const square of gameBoard.gridSquares) {
+        square.addEventListener("click", ()=>{
+            let index = gameBoard.gridSquares.indexOf(square)
+            playRound(index);
+            square.textContent = gameBoard.gameBoard[index]
+        })
+    }
 })();
